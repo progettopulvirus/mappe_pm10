928056221944a3095eec949e16d4462bdf2619ab
guido <progettopulvirus@gmail.com>
2021-11-22 15:37:49 +0100

    Programma INLA per mese

--- R/modello_inla/utilities.R
+++ R/modello_inla/utilities.R
@@ -1,8 +1,14 @@
+
+
+#funzione per standardizzare le covariate spazio temporali
+# @input: .x
+# @output: una lista con: un dataframe con le colonne standardizzate, un dataframe con le medie
 standarizza_covariate<-function(.x){
   
   purrr::map_dfc(.x,.f=mean,na.rm=TRUE)->medie
   purrr::map_dfc(.x,.f=sd,na.rm=TRUE)->deviazioni_standard
   purrr::pmap_dfc(.l=list(.dati=.x,.medie=medie,.sd=deviazioni_standard),.f=function(.dati,.medie,.sd){(.dati-.medie)/.sd})->df
+  
   
   list(.x=df,.medie=medie,.sd=deviazioni_standard)
   
@@ -32,9 +38,11 @@ salva_parametri_per_standardizzazione<-function(.conn,.medie,.sd,.anno,.force=FA
   
   RSQLite::dbExistsTable(mydb,"parametri_standardizzazione")->esiste
   
+  paste0("(",str_c(paste0("'",unique(daSalvare$covariata),"'"),collapse=","),")")->COVARIATE
+
   if(esiste){
     
-    dbGetQuery(.conn,glue::glue('SELECT * FROM parametri_standardizzazione WHERE "yy" = {.anno};'))->tabella
+    dbGetQuery(.conn,glue::glue('SELECT * FROM parametri_standardizzazione WHERE "yy" = {.anno} AND "covariata" IN {COVARIATE};'))->tabella
     
     if(nrow(tabella) & !.force) stop(glue::glue("La tabella if(nrow(tabella) & !.force) già contiene i valori per l'anno {.anno}! Soluzione: .force=TRUE"))
     if(nrow(tabella) & .force) dbExecute(.conn,glue::glue('DELETE FROM parametri_standardizzazione WHERE "yy" = {.anno};'))
